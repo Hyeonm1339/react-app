95937920e809a680377e00b8d70813e09009b903
Hyeonm1339 <jhjimess@naver.com>
2022-06-29 13:39:02 +0900

    change state app.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import React, { Component } from 'react';
 import TOC from './components/TOC';
 
 class App extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: 'WEB', sub: 'World Wide Web' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTMO is for informaion' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' }
+      ]
+    }
+  }
   render() {
     return (
       <div>
-        <Subject title="WEB" sub="world wide web" />
-        <TOC />
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub} />
+        <TOC data={this.state.contents} />
         <Content title="HTML" sub="HTML is HyperText Markup Language" />
       </div>
     )
